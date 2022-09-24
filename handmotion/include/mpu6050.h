38bf3338cc3c7291491d51a3826ee7e938bc376a
0x6f736f646f <blackd0t@protonmail.com>
2022-09-24 16:33:42 +0300

    :sparkles: Add MPU header
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

---
+++ handmotion/include/mpu6050.h
@@ -0,0 +1,41 @@
+#ifndef MPU6050_H
+#define MPU6050_H
+#include "defs.h"
+
+
+class SensorReadings
+{
+private:
+    // This is the sensor Data from hand motion controller
+    // It contains the pitch, roll, yaw and state values
+    Data sensorData;
+
+public:
+    // SensorReadings This initializes the MPU6050 sensor onboard
+    SensorReadings(/* args */);
+    ~SensorReadings();
+
+    // Read picth, roll and yaw values from the MPU6050 sensor
+    void Read();
+
+    // PrintData used for debugging purposes
+    // It prints out the sensorData on the console
+    void PrintData();
+
+    // SendData sends the sensorData over wifi using mqtt protocol
+    void SendData();
+
+    // getPitch gets the pitch of the handmotion sensor
+    float getPitch();
+
+    // getRoll gets the roll of the handmotion sensor
+    float getRoll();
+
+    // getYaw gets the yaw of the handmotion sensor
+    float getYaw();
+
+    // getState gets the state of the handmotion sensor
+    int getState();
+};
+
+#endif
