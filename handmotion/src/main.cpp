d5d6694793150c898fd145bc74df8c1eccc04f21
0x6f736f646f <blackd0t@protonmail.com>
2022-09-25 13:38:18 +0300

    demo
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

--- handmotion/src/main.cpp
+++ handmotion/src/main.cpp
@@ -1,4 +1,14 @@
 #include "Arduino.h"
+#include "../include/mpu6050.h"
+#include "../include/connection.h"
+#include "Adafruit_MQTT.h"
+
+#define LED_BUILTIN 2
+
+WiFiClient client;
+SensorReadings sread;
+Adafruit_MQTT_Publish pubtopic;
+Connection conn(client);
 
 void setup()
 {
@@ -14,6 +24,25 @@ void loop()
   delay(1000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
+  // wait for a second
   delay(1000);
+}
+
+void SetupTopic(Adafruit_MQTT_Client mqtt)
+{
+  char PubBuf[50];
+  PUBTOPIC.toCharArray(PubBuf, 50);
+  pubtopic = Adafruit_MQTT_Publish(&mqtt, PubBuf);
+}
+
+void Connection::SendData(Data payload)
+{
+    if (!pubtopic.publish("payload"))
+    {
+        debugln(F("Failed"));
+    }
+    else
+    {
+        debugln(F("OK!"));
+    }
 }
