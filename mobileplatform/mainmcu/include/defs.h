674e3f9e9fbcae305ed7027aa1b0c97d6458a51d
0x6f736f646f <blackd0t@protonmail.com>
2022-09-30 07:38:41 +0300

    Initial commit
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

--- mobileplatform/mainmcu/include/defs.h
+++ mobileplatform/mainmcu/include/defs.h
@@ -1,25 +1,16 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#include <Arduino.h>
-
-#define DEBUG 2
-#if DEBUG == 1
-#define debug(x) Serial.print(x)
-#define debugln(x) Serial.println(x)
-#define debugf(x, y) Serial.printf(x, y)
-#else
-#define debug(x)
-#define debugln(x)
-#define debugf(x, y)
-#endif
+#define MAXIMUM_BUFFER_SIZE 18
 
 // This is data used by the clients mobile and hand motion sensors
+// For example 0200.1200.1000.h.m
 struct Data
 {
-    float pitch; // Rotation around the side-to-side axis
-    float roll;  // Rotation around the front-to-back axis
-    float yaw;   // Rotation around the vertical axis
+    int pitch;         // Rotation around the side-to-side axis. Int 0 to 2000 i.e 0200. Witch corresponds to -1.00 to +1.00
+    int roll;          // Rotation around the front-to-back axis. Int 0 to 2000 i.e 0200. Witch corresponds to -1.00 to +1.00
+    int yaw;           // Rotation around the vertical axis. Int 0 to 2000 i.e 0200. Witch corresponds to -1.00 to +1.00
+    char publihser[1]; // The publisher of the message. It can be handmotion or mobile. String either h or m
+    char protocol[1];  // The protocol used to send the message. It can be websocket, http or mqtt. String either w, h or m
 };
-
 #endif
--- mobileplatform/mainmcu/include/uart.h
+++ mobileplatform/mainmcu/include/uart.h
@@ -2,20 +2,28 @@
 #define UART_H
 
 #include "defs.h"
+#include "mbed.h"
+// #include "BufferedSerial.h"
+
+// TODO add "AsynchSerial/AsynchSerial.h"
 
 class Uart
 {
 private:
     /* data */
-    int rx_pin;
-    int tx_pin;
-    char format_data(Data data);
+    PinName _rx_pin;
+    PinName _tx_pin;
+    int _baudrate;
+    int _buffer_size = MAXIMUM_BUFFER_SIZE;
+    char* _message_buffer[MAXIMUM_BUFFER_SIZE] = {0};
+    BufferedSerial _esp_serial_port;
+    Data _format_data(char buffer[MAXIMUM_BUFFER_SIZE]);
+
 public:
-    Uart(/* args */);
-    void init(int rx, int tx, int baudrate);
-    void ReceiveData(Data data);
+    Uart(PinName rx, PinName tx, int baudrate);
+    void init();
+    Data ReceiveData();
     ~Uart();
 };
-
 
 #endif
