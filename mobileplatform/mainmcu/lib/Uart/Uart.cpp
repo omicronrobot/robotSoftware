a3cc7332b6a36f106acc1c3f017288831135e37c
0x6f736f646f <blackd0t@protonmail.com>
2022-10-26 09:51:58 +0300

    :sparkles:  fix if error
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

--- mobileplatform/mainmcu/lib/Uart/Uart.cpp
+++ mobileplatform/mainmcu/lib/Uart/Uart.cpp
@@ -9,11 +9,12 @@ Uart::Uart(PinName tx, PinName rx, uint16_t baudrate) : _baudrate(baudrate), _es
         1);
 }
 
+// TODO add support for message pack or json
 Data Uart::ReceiveData()
 {
     int16_t len = _esp_serial_port.read(this->_message_buffer, sizeof(this->_message_buffer));
 
-    if (len > 0) && (len == 4)
+    if ((len > 0) && (len == 4))
     {
         this->_message.pitch = this->_message_buffer[0:2];
         this->_message.yaw = this->_message_buffer[3];
